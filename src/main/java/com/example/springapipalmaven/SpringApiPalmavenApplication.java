29f6f1edd4da4783f2f218c0a20557f2468c9650
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-15 13:29:52 -0300

    first commit

---
+++ src/main/java/com/example/springapipalmaven/SpringApiPalmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapipalmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiPalmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiPalmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springapipalmaven/SpringApiPalmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapipalmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiPalmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
