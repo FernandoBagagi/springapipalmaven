6584f67f8460d4cf62f1c20f795feb32e56faa52
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-23 14:58:35 -0300

    cria aptidão enum

---
+++ src/main/java/com/example/springapipalmaven/model/AptidaoEnum.java
@@ -0,0 +1,16 @@
+package com.example.springapipalmaven.model;
+
+public enum AptidaoEnum {
+    ACENDER_FOGO,
+    PLANTIO,
+    TRABALHO_MANUAL,
+    CORTAR_ARVORES,
+    MANIPULACAO,
+    TRANSPORTE,
+    REGA,
+    GERACAO_DE_ENERGIA,
+    COLETA,
+    GARIMPO,
+    REFRIGERACAO,
+    FAZENDA
+}
