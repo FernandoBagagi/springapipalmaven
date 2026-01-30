95c036003fba6a5a253717f80a581a2ac47458b0
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-30 16:12:06 -0300

    muda a tela home da versão web

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,2 +1,20 @@
 
 console.log("JS carregado com sucesso");
+
+function completarComZero(el) {
+
+    const texto = el.textContent.trim();
+
+    const pad = texto.endsWith('B') ? 4 : 3;
+    
+    el.textContent = texto.padStart(pad, "0");
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document
+        .querySelectorAll(".container-numero h2")
+        .forEach(completarComZero);
+
+});
