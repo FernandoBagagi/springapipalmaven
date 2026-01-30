21f8ae30dffd3d441f226bfd54433d73b0490928
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-30 12:55:55 -0300

    adiciona id na inserção das aptidões

--- src/main/resources/db/migration/V5__insert_aptidoes.sql
+++ src/main/resources/db/migration/V5__insert_aptidoes.sql
@@ -1,16 +1,16 @@
 
 INSERT INTO
-    `aptidoes` (`codigo_enum`, `nome`)
+    `aptidoes` (`id`,`codigo_enum`, `nome`)
 VALUES
-    ('ACENDER_FOGO', 'Acender fogo'),
-    ('PLANTIO', 'Plantio'),
-    ('TRABALHO_MANUAL', 'Trabalho manual'),
-    ('CORTAR_ARVORES', 'Cortar árvores'),
-    ('MANIPULACAO', 'Manipulação'),
-    ('TRANSPORTE', 'Transporte'),
-    ('REGA', 'Rega'),
-    ('GERACAO_DE_ENERGIA', 'Geração de energia'),
-    ('COLETA', 'Coleta'),
-    ('GARIMPO', 'Garimpo'),
-    ('REFRIGERACAO', 'Refrigeração'),
-    ('FAZENDA', 'Fazenda');
+    (1,'ACENDER_FOGO', 'Acender fogo'),
+    (2,'PLANTIO', 'Plantio'),
+    (3,'TRABALHO_MANUAL', 'Trabalho manual'),
+    (4,'CORTAR_ARVORES', 'Cortar árvores'),
+    (5,'MANIPULACAO', 'Manipulação'),
+    (6,'TRANSPORTE', 'Transporte'),
+    (7,'REGA', 'Rega'),
+    (8,'GERACAO_DE_ENERGIA', 'Geração de energia'),
+    (9,'COLETA', 'Coleta'),
+    (10,'GARIMPO', 'Garimpo'),
+    (11,'REFRIGERACAO', 'Refrigeração'),
+    (12,'FAZENDA', 'Fazenda');
