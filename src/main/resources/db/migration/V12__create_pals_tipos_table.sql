3073c6fd1991a1882e24d760b42ef234a447bda0
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-30 12:58:38 -0300

    script de criação da tabela de pals_tipos

---
+++ src/main/resources/db/migration/V12__create_pals_tipos_table.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE `pals_tipos` (
+    
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `pal_id` BIGINT UNSIGNED NOT NULL,
+    `tipo_id` BIGINT UNSIGNED NOT NULL,
+    
+    PRIMARY KEY (`id`),
+    UNIQUE (`pal_id`, `tipo_id`),
+    
+    CONSTRAINT `fk_pals_tipos_pal_id`
+      FOREIGN KEY (`pal_id`)
+      REFERENCES `pals` (`id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+
+    CONSTRAINT `fk_pals_tipos_tipo_id`
+      FOREIGN KEY (`tipo_id`)
+      REFERENCES `tipos` (`id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+
+  );
