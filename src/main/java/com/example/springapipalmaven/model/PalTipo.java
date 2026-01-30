fba957af5bb5645fc740f3687124775ad92a388f
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-30 12:55:04 -0300

    faz relação entre Pal e Tipo

---
+++ src/main/java/com/example/springapipalmaven/model/PalTipo.java
@@ -0,0 +1,40 @@
+package com.example.springapipalmaven.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pals_tipos", uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "pal_id", "tipo_id" })
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class PalTipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "pal_id")
+    private Pal pal;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "tipo_id")
+    private Tipo tipo;
+
+}
